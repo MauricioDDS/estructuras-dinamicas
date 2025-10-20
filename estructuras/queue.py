17d18e38b9d5a6040f8a4015f12dbc323015671f
Mauricio Di Donato Sánchez <127999342+MauricioDDS@users.noreply.github.com>
2025-10-20 07:53:47 -0500

    Implement DoubleNode and enqueue method in Queue
    Implemented the DoubleNode class and the enqueue method for the Queue class, allowing for insertion of elements in a double linked queue.

--- estructuras/queue.py
+++ estructuras/queue.py
@@ -12,17 +12,37 @@ Sugerencia:
 
 class DoubleNode:
     # TODO: implementar nodo doble
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
 
 class Queue:
+    def __init__(self):
+        self.size = 0
+        self.front = None
+        self.rear = None
+        
     # TODO: implementar cola enlazada doble
     def enqueue(self, value):
         """Inserta al final. O(1)"""
-        raise NotImplementedError
+        new_node = DoubleNode(value)
+        if self.rear is None:
+            self.front = self.rear = self.value
+            self.size += 1
+            return
+        self.rear.next = new_node
+        new_node.prev = self.rear
+        self.rear = new_node
+        self.size += 1
+            
 
     def dequeue(self):
         """Extrae el primero. O(1). Debe lanzar IndexError si está vacía."""
-        raise NotImplementedError
+        if self.head:
+            temp = self.head
+        else:
+            raise IndexError
 
     def peek(self):
         """Retorna el primero sin extraer. O(1). IndexError si vacía."""
